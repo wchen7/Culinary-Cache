a3a5c3ca0763d7509ee2d65a7e11d19385deb7f7
William Chen <wchen77771@gmail.com>
2023-08-29 22:25:14 +1000

    created a pantry render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='curtain'>
+      <div className='curtain__wrapper'>
+        <input type="checkbox" defaultChecked/>
+        <div className="curtain__panel curtain__panel--left">
+        </div>
+        <div className='curtain__prize'>
+          Hello
+        </div>
+        <div className='curtain__panel curtain__panel--right'>
+        </div>
+      </div>
     </div>
   );
 }
