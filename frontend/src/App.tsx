06e111f472f90b1862e7fd8ca8ee1746ec833ed7
William Chen <wchen77771@gmail.com>
2023-08-29 23:29:40 +1000

    saving

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [close, setClose] = React.useState<boolean | undefined>(true);
   return (
     <div className='curtain'>
       <div className='curtain__wrapper'>
-        <input type="checkbox" defaultChecked/>
+        <input type="checkbox" checked={close} onClick={(): void => {setClose(!close)}}/>
         <div className="curtain__panel curtain__panel--left">
+          <div className='handle' />
         </div>
         <div className='curtain__prize'>
           Hello
         </div>
         <div className='curtain__panel curtain__panel--right'>
+          <div className='handle' />
         </div>
       </div>
     </div>
